68bb7c6a48b869ffddf6f743e545a1622bd6b761
jiito <benjamin.allanrahill@gmail.com>
2022-06-15 18:51:27 -0400

    initial commit of work page

---
+++ pages/work.tsx
@@ -0,0 +1,40 @@
+const WorkPage = () => {
+    return (
+        <div className="main">
+            <div className="construction">
+                🚧 This page is currently under construction 🚧
+            </div>
+            <h1>Benjamin Allan-Rahill</h1>
+            <div className="nav">
+                <span> work </span>
+                <span> dev</span>
+                <span> writing</span>
+                <span> art</span>
+            </div>
+
+            <div>
+                <h1>prev: </h1>
+            </div>
+            <div>
+                <a href="https://crossmint.io">
+                    <h5> Crossmint </h5>
+                </a>
+                <span>The fastest way to buy NFTs</span>
+                <ul>
+                    <li>
+                        <a href="https://github.com/Crossmint/solana-auth">
+                            Solana Auth
+                        </a>
+                    </li>
+                    <li>WalletConnect Adapter</li>
+                </ul>
+            </div>
+            <div>
+                <a href="https://pallet.com">
+                    <h5> Pallet </h5>
+                </a>
+            </div>
+        </div>
+    )
+}
+export default WorkPage
