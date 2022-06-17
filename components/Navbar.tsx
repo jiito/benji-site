3e39a2d9d606bd4313b02db5d22f39ce27e4cc1b
jiito <benjamin.allanrahill@gmail.com>
2022-06-17 09:52:45 -0400

    adding const links to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,32 @@
+import Link from 'next/link'
+
+const NAVLINKS = [
+    {
+        name: 'work',
+        link: '/work',
+    },
+    {
+        name: 'dev',
+        link: 'https://github.com/jiito',
+    },
+    {
+        name: 'writing',
+        link: 'https://words.benji.ar/',
+    },
+]
+
 export const NavBar = () => {
     return (
         <>
-            <nav>
-                <div className="nav">
-                    <span> work </span>
-                    <span> dev</span>
-                    <span> writing</span>
-                    <span> art</span>
-                </div>
-            </nav>
+            <div className="nav">
+                <nav>
+                    {NAVLINKS.map((item, i, arr) => (
+                        <Link href={item.link} key={i}>
+                            {'/' + item.name}
+                        </Link>
+                    ))}
+                </nav>
+            </div>
         </>
     )
 }
