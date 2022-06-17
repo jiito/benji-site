f86b4630ecd612b153045f1df10534c962064594
jiito <benjamin.allanrahill@gmail.com>
2022-06-17 09:53:10 -0400

    cleaning up home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,7 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-    return (
-        <div className="main">
-            <div className="construction">
-                🚧 This page is currently under construction 🚧
-            </div>
-            <h1>Benjamin Allan-Rahill</h1>
-            <div className="nav">
-                <Link href="/work">
-                    <span> work </span>
-                </Link>
-                <Link href="https://github.com/jiito">
-                    <span> dev </span>
-                </Link>
-
-                <Link href="https://words.benji.ar">
-                    <span> writing</span>
-                </Link>
-            </div>
-        </div>
-    )
+    return <h1>Benjamin Allan-Rahill</h1>
 }
 
 export default Home
