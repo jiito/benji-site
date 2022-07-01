f61c4c38d3716efbf323c7b8e01c9cbdc9883423
jiito <benjamin.allanrahill@gmail.com>
2022-06-30 21:24:27 -0400

    adding raytracer project files

--- pages/work.tsx
+++ pages/work.tsx
@@ -27,6 +27,23 @@ const WorkPage = () => {
                         Unlocking Talent In Internet Communities
                     </span>
                 </div>
+                <div>
+                    <a href="https://pallet.com">
+                        <h5> The Jackson Laboratory </h5>
+                    </a>
+                    <span className="tagline">Biomedical research</span>
+                    <ul>
+                        <li> Paper: tRNA synthetase genes in DO mice. </li>
+                    </ul>
+                </div>
+                <div>
+                    <a href="https://pallet.com">
+                        <h5> Product Buds </h5>
+                    </a>
+                    <span className="tagline">
+                        The largest community of aspiring product managers
+                    </span>
+                </div>
             </div>
         </Layout>
     )
