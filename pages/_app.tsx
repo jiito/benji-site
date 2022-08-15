3eac98497691e55cb3a11d984c5b0133ea8abd65
jiito <benjamin.allanrahill@gmail.com>
2022-08-15 11:21:55 -0600

    feat: g analytics document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,16 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return <>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0Z52XETCEJ"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-0Z52XETCEJ');
+        </script>
+        <Component {...pageProps} /></>;
 }
 
 export default MyApp;
