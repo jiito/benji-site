0b91f9917ee2151935b7f98fa442002dd651cc0f
jiito <benjamin.allanrahill@gmail.com>
2022-08-15 11:23:30 -0600

    fix: removing script app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0Z52XETCEJ"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-0Z52XETCEJ');
-        </script>
-        <Component {...pageProps} /></>;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
