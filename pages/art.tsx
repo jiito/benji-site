df38bf51e69ae0dd14d17fd3f3db7a63a939104a
jiito <benjamin.allanrahill@gmail.com>
2022-11-16 21:18:38 -0500

    formatting

--- pages/art.tsx
+++ pages/art.tsx
@@ -1,22 +1,19 @@
-import Image from "next/image"
-import { Layout } from "../components/Layout"
+import Image from "next/image";
+import { Layout } from "../components/Layout";
 const ArtPage = () => {
-
-
-    return (
-        <Layout>
-            <div>
-                <div className="construction" >
-                    🚧 <em><strong>ROAD WORK AHEAD:</strong></em> This page is currently under construction 🚧
-                </div>
-                <Image
-                    src="/blue_balls.png"
-                    height={500}
-                    width={500}
-                />
-            </div>
-        </Layout>
-    )
-
-}
-export default ArtPage
+  return (
+    <Layout>
+      <div>
+        <div className="construction">
+          🚧{" "}
+          <em>
+            <strong>ROAD WORK AHEAD:</strong>
+          </em>{" "}
+          This page is currently under construction 🚧
+        </div>
+        <Image src="/blue_balls.png" height={500} width={500} />
+      </div>
+    </Layout>
+  );
+};
+export default ArtPage;
