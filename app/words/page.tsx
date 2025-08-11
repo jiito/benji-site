7eef59de11eeef03833ed2226afbf357c3205b69
jiito <benjamin.allanrahill@gmail.com>
2025-08-10 22:33:44 -0700

    add substack link

--- app/words/page.tsx
+++ app/words/page.tsx
@@ -1,9 +1,14 @@
 
 export default function WordsPage() {
   return (
+      <>
       <p>
         I write a (semi) weekly update to a close group of friends. If you feel
         neglected, shoot me an email.
+
+
       </p>
+      <p>some more public writings can be found on my <a href="https://bjar.substack.com">substack</a></p>
+      </>
   );
 }
